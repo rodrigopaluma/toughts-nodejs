ff4915fba7bfe5178e1d1e456436d047e91768d3
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-07-13 08:18:21 -0300

    Implementação do método de Criação de Pensamento.

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -16,4 +16,20 @@ module.exports = class ToughtController {
     static async createTought(req, res) {
         res.render('toughts/create');
     }
+
+    static async createToughtSave(req, res) {
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid
+        };
+        try {
+            await Tought.create(tought);
+            req.flash('message', 'Pensamento criado com sucesso!');
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard');
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
--- routes/toughtsRouter.js
+++ routes/toughtsRouter.js
@@ -6,6 +6,7 @@ const ToughtsController = require('../controllers/ToughtsController');
 const checkAuth = require('../helpers/auth').checkAuth;
 
 router.get('/add', checkAuth, ToughtsController.createTought);
+router.post('/add', checkAuth, ToughtsController.createToughtSave);
 router.get('/dashboard', checkAuth, ToughtsController.dashboard);
 router.get('/', ToughtsController.showToughts);
 
