d54890dc1008146a3e1c5f55f2693513689ace22
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-07-13 17:17:18 -0300

    Criação do form de edição de pensamento e post de update de pensamento.

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -20,7 +20,16 @@ module.exports = class ToughtController {
             res.redirect('/login');
         }
         const toughts = user.Toughts.map((result) => result.dataValues);
-        res.render('toughts/dashboard', { toughts });
+
+        let emptyToughts = false;
+
+        if(toughts.length == 0) {
+            emptyToughts = true;
+        }
+
+        //console.log(toughts);
+
+        res.render('toughts/dashboard', { toughts, emptyToughts });
     }
 
     static async createTought(req, res) {
@@ -35,6 +44,52 @@ module.exports = class ToughtController {
         try {
             await Tought.create(tought);
             req.flash('message', 'Pensamento criado com sucesso!');
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard');
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async removeTought(req, res) {
+        const id = req.body.id;
+        const UserId = req.session.userid;
+
+        try {
+            await Tought.destroy({ where: { id: id, UserId: UserId } });
+            req.flash('message', 'Pensamento removido com sucesso!');
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard');
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async updateTought(req, res) {
+        const id = req.params.id;
+        const userId = req.session.userid;
+
+        const tought = await Tought.findOne({ where: { id: id, UserId: userId }, raw: true });
+        if(!tought) {
+            res.redirect('toughts/dashboard');
+        }
+
+        res.render('toughts/edit', { tought });
+    }
+
+    static async updateToughtSave(req, res) {
+        const id = req.body.id;
+        const userId = req.session.userid;
+
+        const tought = {
+            title: req.body.title
+        };
+
+        try {
+            await Tought.update(tought, { where: { id: id, UserId: userId } });
+            req.flash('message', 'Pensamento atualizado com sucesso!');
             req.session.save(() => {
                 res.redirect('/toughts/dashboard');
             })
